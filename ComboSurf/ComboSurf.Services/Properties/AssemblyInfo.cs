f568bc554602508edc03870aa02e0f423033028a
Tom Dane <tjdane@gmail.com>
2015-08-19 22:56:55 +1000

    create server

--- ComboSurf/ComboSurf.Services/Properties/AssemblyInfo.cs
+++ ComboSurf/ComboSurf.Services/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ComboSurf")]
+[assembly: AssemblyTitle("ComboSurf.Services")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ComboSurf")]
+[assembly: AssemblyProduct("ComboSurf.Services")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b7e93c81-0fba-4764-85d1-98554293f043")]
+[assembly: Guid("c04dd5ec-a570-47f3-a71d-cef9d0674cac")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ComboSurf/ComboSurf.Services/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+namespace ComboSurf.Services
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+            configuration.MapHttpAttributeRoutes();
+            app.UseWebApi(configuration);
+        }
+    }
+}
