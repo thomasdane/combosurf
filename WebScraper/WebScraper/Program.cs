aee32f1824792688556c2d6874a2a11f0fa03058
TomDane <tjdane@gmail.com>
2015-09-30 21:11:58 +1000

    Revert "remove scrapers"
    This reverts commit 2e1bf39fcccb2d37ed9c8ffc32b2727024edd908.

---
+++ WebScraper/WebScraper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coastalwatch = WebScraper.Web.Scrape();
+            Database.Persist(coastalwatch);
+            //var client = new MongoClient();
+            //var database = client.GetDatabase("mixwave");
+            //var coastalwatch = database.GetCollection<Entity>("reports");
+            //coastalwatch.InsertOneAsync(new Entity { Name = "magicseaweed", Content = "testing" });
+        }
+
+        public class Entity
+        {
+            public ObjectId Id { get; set; }
+            public string Name { get; set; }
+            //public HtmlAgilityPack.HtmlDocument Content { get; set; }
+            public string Content { get; set; }
+        }
+    }
+}
