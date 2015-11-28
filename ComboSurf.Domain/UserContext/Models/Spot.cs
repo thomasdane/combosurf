b03e8485c053fa04f503807b3f67120f86d231da
TomDane <tjdane@gmail.com>
2015-11-28 14:14:37 +1100

    implement repository for spots

---
+++ ComboSurf.Domain/UserContext/Models/Spot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboSurf.Domain.UserContext.Models
+{
+	public class Spot
+	{
+		//field
+		public string Name { get; private set; }
+
+		//constructor
+		private Spot(string name)
+		{
+			Name = name;
+			
+		}
+
+		//factory method
+		public static Spot Create(string name)
+		{
+			return new Spot(name);
+		}
+	}
+}
---
+++ ComboSurf.Infrastructure/SpotRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComboSurf.Domain.Models;
+using ComboSurf.Domain.Repositories;
+using ComboSurf.Domain.UserContext.Models;
+
+
+namespace ComboSurf.Infrastructure
+{
+	class SpotRepository : ISpotRepository
+	{
+		public Spot GetByName(string name)
+		{
+			string[] array = { "4ft", "East", "18s", "Gentle", "Offshore", "Rising Tide" };
+			var report = Spot.Create("Manly");
+			return report;
+		}
+	}
+}
